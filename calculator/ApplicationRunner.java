455f35dbfb1d64cf1759e402b1f4ac6940107f92
FANIEL ABRAHAM <fanuelsamsom44@gmail.com>
2022-10-06 10:48:36 +0400

    Stage IV V3.0

--- calculator/ApplicationRunner.java
+++ calculator/ApplicationRunner.java
@@ -2,7 +2,6 @@ package calculator;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        //System.out.println(Parser.infixToPost("15.4\u002B4-3\u00D72\u002B6"));
 
        new Calculator();
     }
